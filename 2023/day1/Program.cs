b0c1b2f8d2d781eb02afe8e28e3bd4400beeb9c5
Pat Roots <patlu6@gmail.com>
2023-12-03 00:19:11 +0100

    Refacto day1-1

--- 2023/day1/Program.cs
+++ 2023/day1/Program.cs
@@ -7,66 +7,68 @@ namespace Aoc
     {
         public static void Main(string[] args) 
         {
-            string arg = args[0];
-            bool isTest;
-
-            bool isArgOk = Boolean.TryParse(arg, out isTest);
-
-            if (!isArgOk) {
-                throw new ArgumentException("Argument must be true for test or false for live");
-            }
+            string[] prefix = getSuffix(args);
             
-            string[] textInput = getStringInput(isTest);
+            string[] textInput = getStringInput(prefix);
 
             int sum = 0;
 
             foreach (string line in textInput) {
-                int startIndex = 0;
-                int endIndex = line.Length -1;
-                int firstDigit = 0, lastDigit = 0;
-                bool digitsFound = false, firstDigitFound = false, lastDigitFound = false;
-
-                while(!digitsFound && (startIndex <= endIndex)) {
-
-                    if (!firstDigitFound) {
-                        
-                        int firstDigitDouble = (int) Char.GetNumericValue(line[startIndex]);
-
-                        if (firstDigitDouble != -1) {
-                            firstDigit = firstDigitDouble;
-                            firstDigitFound = true;
-                        } else {
-                            startIndex ++;
-                        }
-                    }
-
-                    if (!lastDigitFound) {
-                        int lastDigitDouble = (int) Char.GetNumericValue(line[endIndex]);
-
-                        if (lastDigitDouble != -1) {
-                            lastDigit = lastDigitDouble;
-                            lastDigitFound = true;
-                        } else {
-                            endIndex --;
-                        }
-                    }
-
-                    digitsFound = firstDigitFound && lastDigitFound;
-                }
-
-                if (digitsFound) {
-                    sum += firstDigit * 10 + lastDigit;
-                }
+                sum += calculateLine(line);
             }
             
             Console.WriteLine($"Result : {sum}");
         }
 
-        public static string[] getStringInput(bool isTest) {
-            string filePath = isTest ? "./testInput.txt" : "./input.txt";
-            
+        public static int calculateLine(string line) {
+            int startIndex = 0;
+            int endIndex = line.Length -1;
+            int firstDigit = 0, lastDigit = 0;
+            bool digitsFound = false, firstDigitFound = false, lastDigitFound = false;
+
+            while(!digitsFound && (startIndex <= endIndex)) {
+
+                if (!firstDigitFound) {
+                    
+                    int firstDigitDouble = (int) Char.GetNumericValue(line[startIndex]);
+
+                    if (firstDigitDouble != -1) {
+                        firstDigit = firstDigitDouble;
+                        firstDigitFound = true;
+                    } else {
+                        startIndex ++;
+                    }
+                }
+
+                if (!lastDigitFound) {
+                    int lastDigitDouble = (int) Char.GetNumericValue(line[endIndex]);
+
+                    if (lastDigitDouble != -1) {
+                        lastDigit = lastDigitDouble;
+                        lastDigitFound = true;
+                    } else {
+                        endIndex --;
+                    }
+                }
+
+                digitsFound = firstDigitFound && lastDigitFound;
+            }
+
+            return digitsFound ? firstDigit * 10 + lastDigit : 0;
+        }
+
+        public static string[] getSuffix(string[] args) {
+            string? testSuffix = Array.Find(args, arg => arg.ToLower() == "test");
+            string? puzzleNumber = Array.Find(args, arg => arg == "1" || arg == "2");
+
+            return [testSuffix != null ? $"_{testSuffix}" : "", puzzleNumber != null ? $"_{puzzleNumber}" : "_1"];
+        }
+
+        public static string[] getStringInput(string[] prefix) {
+            string filePath = $"./input{prefix[0]}{prefix[1]}.txt";
 
             string[] readText = File.ReadAllLines(filePath);
+
             return readText;
         }
     }
--- 2023/day1/obj/Debug/net8.0/day1.AssemblyInfo.cs
+++ 2023/day1/obj/Debug/net8.0/day1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("day1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8126fb71023160cdfe225002f61068dcf634e27d")]
 [assembly: System.Reflection.AssemblyProductAttribute("day1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("day1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
