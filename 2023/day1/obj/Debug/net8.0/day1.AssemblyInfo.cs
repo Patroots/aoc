06c143101ee1584503a94c8450694c5d5d252f3c
Pat Roots <patlu6@gmail.com>
2023-12-03 20:50:31 +0100

    day1 second puzzle

--- 2023/day1/obj/Debug/net8.0/day1.AssemblyInfo.cs
+++ 2023/day1/obj/Debug/net8.0/day1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("day1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8126fb71023160cdfe225002f61068dcf634e27d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0c1b2f8d2d781eb02afe8e28e3bd4400beeb9c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("day1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("day1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
